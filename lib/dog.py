91ffd668ca9531649b946183454f898c2b544fe8
mathiasmunene <mathiasmunene@gmail.com>
2025-07-10 04:39:51 +0300

    final commit

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print("Woof!")
+
 
     #Instance method definition
-    pass
+fido = Dog()
+fido.bark()
+fido.sit()
